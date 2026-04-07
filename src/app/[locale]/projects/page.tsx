66101d8420f0476cfbe2a6423a306f704b7c508d
Ern1sl <ernisloshaj52@gmail.com>
2026-04-07 09:13:22 +0200

    migrated project videos to Vercel Blob Storage and removed local large assets

--- src/app/[locale]/projects/page.tsx
+++ src/app/[locale]/projects/page.tsx
@@ -25,10 +25,10 @@ export default async function Projects({
   const t = await getTranslations({ locale, namespace: "Projects" });
 
   const videos = [
-    { src: "/Video (1) (1).mp4", id: 1 },
-    { src: "/Video2 (1) (1).mp4", id: 2 },
-    { src: "/Video3 (1) (1).mp4", id: 3 },
-    { src: "/Video4 (1).mp4", id: 4 },
+    { src: "https://7r6meicnrf6qrvft.private.blob.vercel-storage.com/Video%20%281%29%20%281%29.mp4", id: 1 },
+    { src: "https://7r6meicnrf6qrvft.private.blob.vercel-storage.com/Video2%20%281%29%20%281%29.mp4", id: 2 },
+    { src: "https://7r6meicnrf6qrvft.private.blob.vercel-storage.com/Video3%20%281%29%20%281%29.mp4", id: 3 },
+    { src: "https://7r6meicnrf6qrvft.private.blob.vercel-storage.com/Video4%20%281%29.mp4", id: 4 },
   ];
 
   return (
